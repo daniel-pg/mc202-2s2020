af433f015700c283e82da1b643291e01f45b62f2
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-09-22 11:52:26 -0400

    Criado um arquivo makefile; Corrigido bug na funçao ler_apostas causado por especificador de tipo incorreto; Implementada funçao que conta numero de acertos de cada apostador

--- tarefa01/megasena.c
+++ tarefa01/megasena.c
@@ -18,14 +18,16 @@
 
 void ler_apostas(uint64_t lista_apostas[], unsigned m)
 {
-    int entrada;
+    uint64_t entrada;
 
     for (size_t i = 0; i < m; i++)
     {
+        lista_apostas[i] = 0;
+
         for (size_t j = 0; j < TAMANHO_VOLANTE; j++)
         {
-            scanf(" %d", &entrada);
-            lista_apostas[i] |= (entrada << i);
+            scanf(" %lu", &entrada);
+            lista_apostas[i] |= (entrada << j);
         }
     }
 }
@@ -38,16 +40,48 @@ void ler_sorteados(unsigned lista_sorteados[])
     }
 }
 
+void contar_acertos(unsigned lista_acertos[], uint64_t lista_apostas[], unsigned lista_sorteados[], unsigned m)
+{
+    unsigned acertos;
+
+    for (size_t i = 0; i < m; i++)
+    {
+        acertos = 0;
+
+        for (size_t j = 0; j < NUMEROS_SORTEADOS; j++)
+        {
+            if (( (lista_apostas[i] >> (lista_sorteados[j] - 1)) & 0x01) == 0x01)
+            {
+                acertos++;
+            }
+        }
+
+        lista_acertos[i] = acertos;
+
+    }
+}
+
 int main(void)
 {
     unsigned m;
     double n;
     uint64_t lista_apostas[MAX_APOSTAS];
+    unsigned lista_acertos[MAX_APOSTAS];
     unsigned lista_sorteados[NUMEROS_SORTEADOS];
 
     scanf(" %u %lf", &m, &n);
     ler_apostas(lista_apostas, m);
     ler_sorteados(lista_sorteados);
+    contar_acertos(lista_acertos, lista_apostas, lista_sorteados, m);
+
+    for (size_t i = 0; i < m; i++)
+    {
+        // printf("%lu", lista_apostas[i]);
+        printf("%u", lista_acertos[i]);
+    }
+
+    printf("\n");
+    
 
     return 0;
 }
