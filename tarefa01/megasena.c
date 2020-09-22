e8ae8cf9d6b354e3eed042ccb4c7c328e6157564
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-09-22 17:05:35 -0400

    Documentaçao atualizada!

--- tarefa01/megasena.c
+++ tarefa01/megasena.c
@@ -16,6 +16,14 @@
 #define TAMANHO_VOLANTE 60
 #define NUMEROS_SORTEADOS 6
 
+
+/**
+ * Lê um número m de matrizes binárias, correspondentes aos volantes de cada aposta, da entrada padrão.
+ * 
+ * @param lista_apostas Vetor de tamanho fixo, onde cada elemento corresponde a uma aposta individual a ser lida.
+ * @param m Número de apostas. Deve ser menor ou igual a constante MAX_APOSTAS.
+ * 
+ */
 void ler_apostas(uint64_t lista_apostas[], unsigned m)
 {
     uint64_t entrada;
@@ -27,11 +35,18 @@ void ler_apostas(uint64_t lista_apostas[], unsigned m)
         for (size_t j = 0; j < TAMANHO_VOLANTE; j++)
         {
             scanf(" %lu", &entrada);
-            lista_apostas[i] |= (entrada << j);
+            // * Etapa desnecessária, pois assume-se que a entrada sempre é válida (0 ou 1):
+            // entrada &= 0x01;
+            lista_apostas[i] |= (entrada << j); // Grava o bit de entrada na posição j do número.
         }
     }
 }
 
+/**
+ * Lê uma lista de números sorteados da entrada padrão e os armazena no vetor dado.
+ * 
+ * @param lista_sorteados Vetor onde os valores lidos serão guardados.
+ */
 void ler_sorteados(unsigned lista_sorteados[])
 {
     for (size_t i = 0; i < NUMEROS_SORTEADOS; i++)
@@ -40,6 +55,14 @@ void ler_sorteados(unsigned lista_sorteados[])
     }
 }
 
+/**
+ *  Conta o número de acertos de cada aposta.
+ * 
+ * @param lista_acertos Vetor onde o número de acertos de cada aposta será guardado.
+ * @param lista_apostas Vetor de números binários, cujos bits correspondem aos valores marcados no volante de cada aposta.
+ * @param lista_sorteados Vetor contendo os números sorteados do jogo.
+ * @param m Número de apostas. Deve ser menor ou igual a constante MAX_APOSTAS.
+ */
 void contar_acertos(unsigned lista_acertos[], uint64_t lista_apostas[], unsigned lista_sorteados[], unsigned m)
 {
     unsigned acertos;
@@ -60,6 +83,14 @@ void contar_acertos(unsigned lista_acertos[], uint64_t lista_apostas[], unsigned
     }
 }
 
+/**
+ * Conta quantas apostas acertaram uma determinada quantidade dos números sorteados.
+ * 
+ * @param lista_acertos Vetor contendo o número de acertos de cada aposta.
+ * @param m Número de apostas. Deve ser menor ou igual a constante MAX_APOSTAS.
+ * @param acertos Parâmetro que determina qual a quantidade de acertos de cada aposta contabilizada na contagem.
+ * @return Retorna o número de premiados com a quantidade de acertos especificada. 
+ */
 unsigned contar_premiados(unsigned lista_acertos[], unsigned m, unsigned acertos)
 {
     unsigned premiados = 0;
@@ -75,14 +106,27 @@ unsigned contar_premiados(unsigned lista_acertos[], unsigned m, unsigned acertos
     return premiados;
 }
 
+/**
+ * Lista os valores dos prêmios de cada aposta.
+ * 
+ * @param lista_acertos Vetor contendo o número de acertos de cada aposta.
+ * @param contagem_premiados Vetor contendo o número de premiados na quadra, na quina e na sena, respectivamente.
+ * @param m Número de apostas. Deve ser menor ou igual a constante MAX_APOSTAS.
+ * @param n Valor do prêmio.
+ */
 void listar_premios(unsigned lista_acertos[], unsigned contagem_premiados[], unsigned m, double n)
 {
     double valores_premios[3] = {0.0, 0.0, 0.0};
 
     // Caso não hajam premiados na quadra, quina ou sena, previne uma divisão por zero.
-    if (contagem_premiados[0] > 0) valores_premios[0] = (0.19 * n) / contagem_premiados[0];
-    if (contagem_premiados[1] > 0) valores_premios[1] = (0.19 * n) / contagem_premiados[1];
-    if (contagem_premiados[2] > 0) valores_premios[2] = (0.62 * n) / contagem_premiados[2];
+    if (contagem_premiados[0] > 0)
+        valores_premios[0] = (0.19 * n) / contagem_premiados[0]; // Premiados na quadra
+
+    if (contagem_premiados[1] > 0)
+        valores_premios[1] = (0.19 * n) / contagem_premiados[1]; // Premiados na quina
+
+    if (contagem_premiados[2] > 0)
+        valores_premios[2] = (0.62 * n) / contagem_premiados[2]; // Premiados na sena
 
     for (size_t i = 0; i < m; i++)
     {
