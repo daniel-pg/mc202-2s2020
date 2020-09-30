f57446eaf1c33cf1517d9183cc78ef9a8dd3a096
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-09-30 08:48:24 -0400

    Implementa função que chama as operações.

--- tarefa02/professor_carlos_main.c
+++ tarefa02/professor_carlos_main.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include "professor_carlos.h"
 
-#define MAX_NOME_SOBRENOME 15
 #define MAX_SUBSTRING 6
 #define MAX_TURMAS 50
-#define MAX_ALUNOS 50
-#define MAX_OPERACOES 50
-
 
 void ler_turmas(Turma lista_turmas[], int n)
 {
@@ -18,70 +14,85 @@ void ler_turmas(Turma lista_turmas[], int n)
     for (size_t i = 0; i < n; i++)
     {
         scanf(" %d", &m);
+        // O número de alunos por turma já é dado:
         lista_turmas[i].qtd = m;
 
         for (size_t j = 0; j < m; j++)
         {
-            scanf(" %s %s %d %d %d", &entrada_aluno.nome, &entrada_aluno.sobrenome, &dia, &mes, &ano);
+            scanf(" %s %s %d %d %d", entrada_aluno.nome, entrada_aluno.sobrenome, &dia, &mes, &ano);
             entrada_data.dia = dia;
             entrada_data.mes = mes;
             entrada_data.ano = ano;
             entrada_aluno.nascimento = entrada_data;
             lista_turmas[i].alunos[j] = entrada_aluno;
+            // Precisaria contar os alunos um a um:
+            // add_aluno(lista_turmas, entrada_aluno, i);
         }
     }
 }
 
-
 void faz_operacao(Turma lista_turmas[], int operacao, int n)
 {
-    int j;
+    Aluno aluno_saida;
+    Aluno aluno_entrada;
+    Data data_entrada;
     char padrao[MAX_SUBSTRING];
+    int contagem_substrings;
+    int j;
+    int dia, mes, ano;
+    int qtd_alunos;
 
     switch (operacao)
-        {
-        case 1:
-            scanf(" %d", &j);
-            procura_novo_na_turma(lista_turmas, n, j);
-            // nome
-            break;
+    {
+    case 1:
+        scanf(" %d", &j);
+        aluno_saida = procura_novo_na_turma(lista_turmas, n, j);
+        printf("%s\n", aluno_saida.nome);
+        break;
 
-        case 2:
-            scanf(" %d", &j);
-            procura_velho_na_turma(lista_turmas, n, j);
-            // sobrenome
-            break;
+    case 2:
+        scanf(" %d", &j);
+        aluno_saida = procura_velho_na_turma(lista_turmas, n, j);
+        printf("%s\n", aluno_saida.sobrenome);
+        break;
 
-        case 3:
-            procura_velho_todas_turmas(lista_turmas, n);
-            // nome
-            break;
+    case 3:
+        aluno_saida = procura_velho_todas_turmas(lista_turmas, n);
+        printf("%s\n", aluno_saida.nome);
+        break;
 
-        case 4:
-            procura_novo_todas_turmas(lista_turmas, n);
-            // sobrenome
-            break;
+    case 4:
+        aluno_saida = procura_novo_todas_turmas(lista_turmas, n);
+        printf("%s\n", aluno_saida.sobrenome);
+        break;
 
-        case 5:
-            scanf(" %s", &padrao);
-            conta_substrings(lista_turmas, n, padrao);
-            break;
+    case 5:
+        scanf(" %s", padrao);
+        contagem_substrings = conta_substrings(lista_turmas, n, padrao);
+        printf("%d\n", contagem_substrings);
+        break;
 
-        case 6:
-            /* Dados j, nome, sobrenome, dia, mês, e ano de nascimento, deve-se adicionar
-            um novo aluno contendo o nome, sobrenome, dia, mês e ano de nascimento, informados
-            previamente, na j-ésima turma. Após adicionado, deve-se retornar quantos alunos
-            a turma j possui no momento. */
-            break;
+    case 6:
+        scanf(" %d %s %s %d %d %d", &j, aluno_entrada.nome, aluno_entrada.sobrenome, &dia, &mes, &ano);
+        data_entrada.dia = dia;
+        data_entrada.mes = mes;
+        data_entrada.ano = ano;
+        aluno_entrada.nascimento = data_entrada;
+        qtd_alunos = add_aluno(lista_turmas, aluno_entrada, j);
+        printf("%d\n", qtd_alunos);
+        break;
 
-        case 7:
-            /* Dado j, deve-se remover o ultimo aluno adicionado na turma j. Após removido,
+    case 7:
+        /* Dado j, deve-se remover o ultimo aluno adicionado na turma j. Após removido,
             deve-se retornar quantos alunos a turma j possui no momento. */
-            break;
+        scanf(" %d", &j);
+        qtd_alunos = remove_aluno(lista_turmas, j);
+        printf("%d\n", qtd_alunos);
+        break;
 
-        default:
-            break;
-        }
+    default:
+        break;
+    }
 }
 
 int main(void)
