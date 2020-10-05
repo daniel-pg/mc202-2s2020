5c998cdb15c3aac3e4624bdb280583c3a595b77d
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-10-05 14:50:32 -0400

    Cria esqueleto básico da tarefa 03

---
+++ tarefa03/fakenews.c
@@ -0,0 +1,20 @@
+/**
+ * Daniel Paulo Garcia © 2020
+ *
+ * fakenews.c
+ *
+ * Descrição: ...
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int n, m;
+    scanf(" %d %d", &n, &m);
+
+    return 0;
+}
