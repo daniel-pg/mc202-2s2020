0e046d4433af873babd1244d0866ce475f593063
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-10-05 15:37:02 -0400

    Cria estrutura de dados básica para guardar cada termo, e aloca/libera corretamente uma lista de termos.

--- tarefa03/fakenews.c
+++ tarefa03/fakenews.c
@@ -11,10 +11,63 @@
 #include <stdio.h>
 #include <string.h>
 
+#define MAX_PALAVRA 25
+
+typedef struct termo
+{
+    char *palavra;
+    double *historico;
+} Termo;
+
+Termo * alocaListaTermos(int n, int m)
+{
+    Termo *lista_termos = malloc(sizeof(Termo) * n);
+
+    if (lista_termos == NULL)
+    {
+        goto erro;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        lista_termos[i].palavra = malloc(sizeof(char) * MAX_PALAVRA);
+        lista_termos[i].historico = malloc(sizeof(double) * m);
+
+        if (!lista_termos[i].palavra || !lista_termos[i].historico)
+        {
+            goto erro;
+        }
+
+    }
+
+    return lista_termos;
+
+    erro:
+        printf("Memoria insuficiente!");
+        exit(1);
+}
+
+void liberaListaTermos(Termo *lista_termos, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        free(lista_termos[i].palavra);
+        free(lista_termos[i].historico);
+    }
+
+    free(lista_termos);
+}
+
 int main(void)
 {
+    Termo *lista_termos;
     int n, m;
+
     scanf(" %d %d", &n, &m);
+
+    lista_termos = alocaListaTermos(n, m);
+
+    liberaListaTermos(lista_termos, n);
 
     return 0;
 }
