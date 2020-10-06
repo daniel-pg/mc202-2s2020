317fe101e2109adb929bb4aa2c2f95a9e7cd47a9
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-10-06 19:21:02 -0400

    Implementa leitura dos numeros n, m, p e do caça-palavras da entrada.

--- tarefa04/seaofwords.c
+++ tarefa04/seaofwords.c
@@ -2,3 +2,58 @@
 // Created by danielpg on 06/10/2020.
 //
 
+#include <stdio.h>
+#include <stdlib.h>
+#include "seaofwords.h"
+
+void le_cacapalavras(char *cacapalavras, int n, int m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            scanf(" %c", (cacapalavras + sizeof(char) * (i * n + j)) );
+        }
+    }
+}
+
+/* Função utilizada apenas para depurar o programa e checar se a entrada foi lida corretamente */
+void escreve_cacapalavras(const char *cacapalavras, int n, int m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            printf("%c", *(cacapalavras + sizeof(char) * (i * n + j)) );
+        }
+
+        printf("\n");
+    }
+}
+
+int main(void)
+{
+    int n, m, q;
+    scanf(" %d %d %d", &n, &m, &q); // Assume-se que a entrada sempre é válida, não há necessidade de usar fgets() ou strtol()
+
+    /* Infelizmente o C90 não permite coisas como sizeof(char[n][m]), que é um um uso legítimo e seguro de VLA's.
+     * Todas vez que quiser acessar a matriz, ao invés de usar a sintaxe super-conveniente:     matriz[i][j];
+     * Terei que escrever:      *(matriz + sizeof(char) * (i * n + j))
+     *
+     * Solução elegante, se pudesse usar VLA's:
+     * char (*matriz)[m] = malloc(sizeof(int[n][m]));
+     * */
+    char *cacapalavras = malloc(n * m * sizeof(char));
+    if (cacapalavras == NULL)
+    {
+        printf("Erro de alocacao de memoria!");
+        exit(1);
+    }
+
+    le_cacapalavras(cacapalavras, n, m);
+    escreve_cacapalavras(cacapalavras, n, m);
+
+
+    free(cacapalavras);
+    return 0;
+}
--- tarefa04/seaofwords.h
+++ tarefa04/seaofwords.h
@@ -5,4 +5,7 @@
 #ifndef TAREFA04_SEAOFWORDS_H
 #define TAREFA04_SEAOFWORDS_H
 
+void le_cacapalavras(char *cacapalavras, int n, int m);
+void escreve_cacapalavras(const char *cacapalavras, int n, int m);
+
 #endif //TAREFA04_SEAOFWORDS_H
