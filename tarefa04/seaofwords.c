ba544a364142323683955c1b85f6e66c8077fd8b
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-10-06 19:17:31 -0400

    Cria arquivos da tarefa 04

---
+++ tarefa04/seaofwords.c
@@ -0,0 +1,4 @@
+//
+// Created by danielpg on 06/10/2020.
+//
+
---
+++ tarefa04/seaofwords.h
@@ -0,0 +1,8 @@
+//
+// Created by danielpg on 06/10/2020.
+//
+
+#ifndef TAREFA04_SEAOFWORDS_H
+#define TAREFA04_SEAOFWORDS_H
+
+#endif //TAREFA04_SEAOFWORDS_H
