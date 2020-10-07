b6eb194994a1234da98c3803e05d4a96f17d959f
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-10-07 10:59:57 -0400

    Implementa funções le_pistas, escreve_pistas e imprime_resultados Implementa parcialmente a função procura_palavras Cria função dummy para backtrack (implementar depois) Atualiza cabeçalho seaofwords.h

--- tarefa04/seaofwords.c
+++ tarefa04/seaofwords.c
@@ -2,9 +2,13 @@
 // Created by danielpg on 06/10/2020.
 //
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "seaofwords.h"
+
+#define MAX_PALAVRA 20
 
 void le_cacapalavras(char *cacapalavras, int n, int m)
 {
@@ -14,6 +18,14 @@ void le_cacapalavras(char *cacapalavras, int n, int m)
         {
             scanf(" %c", (cacapalavras + sizeof(char) * (i * n + j)) );
         }
+    }
+}
+
+void le_pistas(char *pistas, int q)
+{
+    for (int i = 0; i < q; i++)
+    {
+        scanf(" %s", &pistas[i]);
     }
 }
 
@@ -31,6 +43,69 @@ void escreve_cacapalavras(const char *cacapalavras, int n, int m)
     }
 }
 
+/* Função utilizada apenas para depurar o programa e checar se a entrada foi lida corretamente */
+void escreve_pistas(char *pistas, int q)
+{
+    for (int i = 0; i < q; i++)
+    {
+        printf("%s\n", &pistas[i]);
+    }
+}
+
+void procura_palavras(const char *cacapalavras, const char *palavras, bool *resultados, int n, int m, int q)
+{
+    char palavra_atual[MAX_PALAVRA];
+
+    // Para cada palavra
+    for (int k = 0; k < q; k++)
+    {
+        // Caso básico: A tabela está vazia
+        if (!n || !m){
+            resultados[k] = 0;
+            continue;
+        }
+
+        strncpy(palavra_atual, &palavras[k], MAX_PALAVRA);
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (backtrack(cacapalavras, palavra_atual, 0, i, j))
+                {
+                    resultados[k] = true;
+                    goto proxima_palavra;
+                }
+            }
+        }
+        resultados[k] = 0;
+
+        proxima_palavra:;
+
+    }
+
+}
+
+bool backtrack(const char *cacapalavras, const char *palavra, int index, int i, int j)
+{
+    return true;
+}
+
+void imprime_resultados(const bool *resultados, int q)
+{
+    for (int i = 0; i < q; i++)
+    {
+        if (resultados[i])
+        {
+            printf("sim\n");
+        }
+        else
+        {
+            printf("nao\n");
+        }
+    }
+}
+
 int main(void)
 {
     int n, m, q;
@@ -43,17 +118,30 @@ int main(void)
      * Solução elegante, se pudesse usar VLA's:
      * char (*matriz)[m] = malloc(sizeof(int[n][m]));
      * */
-    char *cacapalavras = malloc(n * m * sizeof(char));
-    if (cacapalavras == NULL)
+    char *cacapalavras = malloc(sizeof(char) * n * m);
+    char *pistas = malloc(sizeof(char[MAX_PALAVRA]) * q);
+    bool *resultados = malloc(sizeof(bool) * q);
+    if (!cacapalavras || !pistas || !resultados)
     {
+        free(resultados);
+        free(pistas);
+        free(cacapalavras);
         printf("Erro de alocacao de memoria!");
         exit(1);
     }
 
     le_cacapalavras(cacapalavras, n, m);
-    escreve_cacapalavras(cacapalavras, n, m);
+    le_pistas(pistas, q);
+    // DEBUG: Escreve entrada devolta na tela
+    // escreve_cacapalavras(cacapalavras, n, m);
+    // escreve_pistas(pistas, q);
 
+    procura_palavras(cacapalavras, pistas, resultados, n, m, q);
+    imprime_resultados(resultados, q);
 
+    // Libera memoria
+    free(resultados);
+    free(pistas);
     free(cacapalavras);
     return 0;
 }
--- tarefa04/seaofwords.h
+++ tarefa04/seaofwords.h
@@ -6,6 +6,11 @@
 #define TAREFA04_SEAOFWORDS_H
 
 void le_cacapalavras(char *cacapalavras, int n, int m);
+void le_pistas(char *pistas, int q);
 void escreve_cacapalavras(const char *cacapalavras, int n, int m);
+void escreve_pistas(char *pistas, int q);
+void procura_palavras(const char *cacapalavras, const char *palavras, bool *resultados, int n, int m, int q);
+bool backtrack(const char *cacapalavras, const char *palavra, int index, int i, int j);
+void imprime_resultados(const bool *resultados, int q);
 
 #endif //TAREFA04_SEAOFWORDS_H
