b1f8017e1be71a572c980b7d95282a8b42c859da
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-10-24 18:38:26 -0400

    Implementa funções de inicializar, esvaziar e liberar lista.

--- tarefa05/lista_ligada.c
+++ tarefa05/lista_ligada.c
@@ -9,3 +9,38 @@
 #include <stdlib.h>
 
 #include "lista_ligada.h"
+
+lista_ligada_t * inicializa_lista(void)
+{
+    lista_ligada_t *lista;
+    if ( (lista = malloc(sizeof(lista_ligada_t))) == NULL ) exit(1);
+    if ((lista->inicio = malloc(sizeof(celula_t))) == NULL ) exit(1);
+
+    lista->inicio->prox = NULL;
+    lista->inicio->valor = 0;
+    lista->len = 0;
+
+    return lista;
+}
+
+void esvazia_lista(lista_ligada_t *lista)
+{
+    celula_t *anterior;
+
+    for (size_t i = 0; i < lista->len; i++)
+    {
+        anterior = lista->inicio->prox;
+        lista->inicio->prox = anterior->prox;
+        free(anterior);
+    }
+
+    lista->inicio->valor = 0;
+    lista->len = 0;
+}
+
+void libera_lista(lista_ligada_t *lista)
+{
+    esvazia_lista(lista);
+    free(lista->inicio);
+    free(lista);
+}
