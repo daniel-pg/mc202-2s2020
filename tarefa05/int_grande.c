68ba127be2be584a5435358aa980e985558ed3a4
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-10-24 15:23:19 -0400

    Cria protótipo do módulo de manipulação de números grandes.

---
+++ tarefa05/int_grande.c
@@ -0,0 +1,14 @@
+/**
+ * Daniel Paulo Garcia © 2020
+ *
+ * int_grande.c
+ *
+ * Descrição: Implementa funções do módulo int_grande.h
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "int_grande.h"
+
+
---
+++ tarefa05/int_grande.h
@@ -0,0 +1,14 @@
+/**
+ * Daniel Paulo Garcia © 2020
+ *
+ * int_grande.h
+ *
+ * Descrição: Permite manipular números inteiros positivos de precisão arbitrária.
+ */
+
+#ifndef TAREFA05_INT_GRANDE_H
+#define TAREFA05_INT_GRANDE_H
+
+#include "lista_ligada.h"
+
+#endif //TAREFA05_INT_GRANDE_H
