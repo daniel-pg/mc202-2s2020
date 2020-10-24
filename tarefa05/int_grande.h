7da8c70f709719f4eda1ade4b86b1be757b830fd
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-10-24 18:32:07 -0400

    Adiciona algumas ideias de funções sobre listas ligadas que podem ser úteis. Talvez nem todas serão implementadas, a não que sejam necessárias para o módulo int_grande.

--- tarefa05/int_grande.h
+++ tarefa05/int_grande.h
@@ -11,4 +11,16 @@
 
 #include "lista_ligada.h"
 
+// Escreve o número em formato decimal na saída padrão.
+
+// Lê um número em formato decimal da entrada padrão.
+
+// Soma dois números e guarda o resultado na variável de destino.
+
+// Multiplica dois números e guarda o resultado na variável de destino.
+
+// Subtrai dois números e guarda o resultado em módulo na variável de destino.
+
+// Divide dois números e guarda o resultado na variável de destino.
+
 #endif //TAREFA05_INT_GRANDE_H
--- tarefa05/lista_ligada.h
+++ tarefa05/lista_ligada.h
@@ -21,8 +21,29 @@ typedef struct celula
 
 typedef struct lista_ligada
 {
-    celula_t *sentinela;
-    int len;
+    celula_t *inicio;
+    size_t len;
 } lista_ligada_t;
+
+// Aloca e inicializa uma lista ligada na memória.
+lista_ligada_t * inicializa_lista(void);
+
+// Remove todos os elementos da lista
+void esvazia_lista(lista_ligada_t *lista);
+
+// Esvazia a lista e logo em seguida libera-a da memória.
+void libera_lista(lista_ligada_t *lista);
+
+// Insere elemento na posição especificada.
+
+// Anexa elemento ao final da lista.
+
+// Concatena duas listas
+
+// Remove primeira ocorrência do elemento especificado da lista.
+
+// Retira o elemento na posição especificada.
+
+// Copia a lista de origem para a lista de destino.
 
 #endif //TAREFA05_LISTA_LIGADA_H
