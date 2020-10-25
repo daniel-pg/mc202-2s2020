b63c3c8dcd9d03eeac9495f6a4e557721069cb1d
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-10-24 22:17:38 -0400

    Atualiza implementações das funções antigas para funcionar com listas duplamente ligadas.
    Implementa novas funções anexa_elemento, imprime_lista e ler_lista.

--- tarefa05/lista_ligada.c
+++ tarefa05/lista_ligada.c
@@ -6,6 +6,8 @@
  * Descrição: Implementa funções do módulo lista_ligada.h
  */
 
+#include <inttypes.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "lista_ligada.h"
@@ -14,10 +16,9 @@ lista_ligada_t * inicializa_lista(void)
 {
     lista_ligada_t *lista;
     if ( (lista = malloc(sizeof(lista_ligada_t))) == NULL ) exit(1);
-    if ((lista->inicio = malloc(sizeof(celula_t))) == NULL ) exit(1);
 
-    lista->inicio->prox = NULL;
-    lista->inicio->valor = 0;
+    lista->inicio = NULL;
+    lista->fim = NULL;
     lista->len = 0;
 
     return lista;
@@ -29,18 +30,49 @@ void esvazia_lista(lista_ligada_t *lista)
 
     for (size_t i = 0; i < lista->len; i++)
     {
-        anterior = lista->inicio->prox;
-        lista->inicio->prox = anterior->prox;
+        anterior = lista->inicio;
+        lista->inicio = anterior->prox;
         free(anterior);
     }
 
-    lista->inicio->valor = 0;
+    lista->fim = NULL;
     lista->len = 0;
 }
 
 void libera_lista(lista_ligada_t *lista)
 {
     esvazia_lista(lista);
-    free(lista->inicio);
     free(lista);
+}
+
+void anexa_elemento(lista_ligada_t *lista, const item_t item)
+{
+    celula_t *novo_elemento;
+    if ( (novo_elemento = malloc(sizeof(*novo_elemento))) == NULL ) exit(1);
+
+    novo_elemento->valor = item;
+    novo_elemento->ant = lista->fim;
+    novo_elemento->prox = NULL;
+
+    lista->fim = novo_elemento;
+}
+
+void imprime_lista(lista_ligada_t *lista, const char *sep)
+{
+    for (celula_t *atual = lista->inicio; atual != NULL ; atual = atual->prox)
+    {
+        printf("%" PRIu64, atual->valor);
+        printf("%s", sep);
+    }
+}
+
+void ler_lista(lista_ligada_t *lista, size_t n)
+{
+    item_t el;
+
+    for (size_t i = 0; i < n; i++)
+    {
+        scanf(" %" PRIu64, &el);
+        anexa_elemento(lista, el);
+    }
 }
