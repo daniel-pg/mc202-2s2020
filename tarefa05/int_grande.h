e331c87537e6db216e2f5c1518ee73ff1cc8c41f
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-10-27 02:17:59 -0400

    Adiciona protótipos das funções do cabeçalho int_grande.h

--- tarefa05/int_grande.h
+++ tarefa05/int_grande.h
@@ -12,15 +12,21 @@
 #include "lista_ligada.h"
 
 // Escreve o número em formato decimal na saída padrão.
+void imprime_ngrande(lista_ligada_t *n);
 
 // Lê um número em formato decimal da entrada padrão.
+void le_ngrande(lista_ligada_t *n);
 
 // Soma dois números e guarda o resultado na variável de destino.
+lista_ligada_t * soma_ngrande(lista_ligada_t *n1, lista_ligada_t *n2);
 
 // Multiplica dois números e guarda o resultado na variável de destino.
+lista_ligada_t * multiplica_ngrande(lista_ligada_t *n1, lista_ligada_t *n2);
 
 // Subtrai dois números e guarda o resultado em módulo na variável de destino.
+lista_ligada_t * subtrai_ngrande(lista_ligada_t *n1, lista_ligada_t *n2);
 
 // Divide dois números e guarda o resultado na variável de destino.
+lista_ligada_t * divide_ngrande(lista_ligada_t *n1, lista_ligada_t *n2);
 
 #endif //TAREFA05_INT_GRANDE_H
