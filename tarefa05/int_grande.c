6e6969dc5615ea0f8fa637454bf60d3835762d4f
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-11-03 02:28:22 -0400

    Agora as funções de operações numéricas esvaziam a variável de resultado antes de realizar os cálculos; isso previne alguns erros. Implementa função de soma de números grandes. Corrige função de leitura para segmentar o número de entrada corretamente. Corrige função de escrita para imprimir os membros do número alinhados com zeros, exceto para o último membro. Adiciona algumas macros úteis.

--- tarefa05/int_grande.c
+++ tarefa05/int_grande.c
@@ -6,26 +6,48 @@
  * Descrição: Implementa funções do módulo int_grande.h
  */
 
+#include <inttypes.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "int_grande.h"
 
-// Maximo de algarismos decimais que cabem em um int de 64 bits.
-#define MAX_DIG_DECIMAL 19
+// Infelizmente a linguagem C é ruim o suficiente pra nos forçar a esse tipo de truque para tarefas tão simples.
+#define STR(x) #x
+#define XSTR(x) STR(x)
 
-#define MAX_NUM_LENGTH 25
+// Maximo de algarismos decimais que cabem em um int de 64 bits, menos um algarismo para caber o resto da soma.
+#define MAX_DIG_DECIMAL 18
+#define MAX_VAL_DECIMAL (item_t) pow(10, MAX_DIG_DECIMAL)
+
+#define MAX_NUM_LENGTH 64
 
 void imprime_ngrande(lista_ligada_t *num)
 {
-    imprime_lista_inverso(num, "");
+    celula_t *atual = num->fim;
+
+    while (atual != NULL)
+    {
+        // O último caractere deve ser impresso sem zeros à esquerda, e o restante deve ser alinhado à largura de um
+        // membro da lista usando zeros.
+        if (atual->prox == NULL)
+            printf("%" PRIu64, atual->valor);
+        else
+            printf("%0" XSTR(MAX_DIG_DECIMAL) PRIu64, atual->valor);
+
+        atual = atual->ant;
+    }
+
+    printf("\n");
 }
 
 // Algoritmo de conversão de base numérica baseado no algoritmo da biblioteca GMPLib. Adaptado para ler números decimais.
 // Fonte: https://gmplib.org/manual/Radix-to-Binary
 void conv_str_para_bin(lista_ligada_t *num, const char *str, size_t tmh_str)
 {
+    size_t k = 0;
     item_t atual = 0;
     item_t multiplicador = 1;
 
@@ -34,17 +56,21 @@ void conv_str_para_bin(lista_ligada_t *num, const char *str, size_t tmh_str)
         atual += (item_t) (str[i] - '0') * multiplicador;
         multiplicador *= 10;
 
-        if ( (i + 1) % MAX_DIG_DECIMAL == 0 || i == 0)
+        if ((k + 1) % MAX_DIG_DECIMAL == 0 || i == 0)
         {
             anexa_elemento(num, atual);
             atual = 0;
             multiplicador = 1;
         }
+
+        k++;
     }
 }
 
 void le_ngrande(lista_ligada_t *num)
 {
+    esvazia_lista(num);
+
     char entrada[MAX_NUM_LENGTH];
     scanf(" %s", entrada);
     size_t tmh_str = strlen(entrada);
@@ -53,22 +79,70 @@ void le_ngrande(lista_ligada_t *num)
 
 void soma_ngrande(lista_ligada_t *resultado, lista_ligada_t *n1, lista_ligada_t *n2)
 {
+    // Queremos ignorar o valor anterior da lista antes de realizar quaisquer cálculos.
+    esvazia_lista(resultado);
+
     // Garante que a soma sempre seja feita a partir do menor número sobre o maior. Facilita a implementação do algoritmo.
-    if (n1->len > n2->len)
+    // Caso um número seja maior que outro mas ocupe o mesmo espaço na memória, o resultado será o mesmo.
+    if (n1->len > n2->len) {
         copia_lista(resultado, n1);
-    else
+    }
+    else {
         copia_lista(resultado, n2);
+        n2 = n1; // Não é necessário guardar uma referência para n2 uma vez feita a sua cópia.
+    }
+
+    anexa_elemento(resultado, 0); // Cria um espaço a mais para caso a soma ultrapasse o tamanho da lista.
 
     // TODO: Algoritmo de soma aqui
+    item_t resto_soma = 0;
+    celula_t *atual_result, *atual_n2;
+    atual_result = resultado->inicio;
+    atual_n2 = n2->inicio;
+
+    for (size_t i = 0; i < resultado->len; i++)
+    {
+        // Caso estejamos somando os dígitos de n1 e n2 em paralelo...
+        if (i < n2->len) {
+            // É garantido que a soma nunca irá estourar o limite de um item_t devido ao limite MAX_DIG_DECIMAL.
+            atual_result->valor += atual_n2->valor + resto_soma;
+            atual_n2 = atual_n2->prox;
+        }
+        else {
+            // ...senão, devemos apenas propagar o resto da soma.
+            atual_result->valor += resto_soma;
+        }
+
+        if (atual_result->valor >= MAX_VAL_DECIMAL) {
+            atual_result->valor -= MAX_VAL_DECIMAL;
+            resto_soma = 1;
+        }
+        else {
+            resto_soma = 0;
+        }
+
+        atual_result = atual_result->prox;
+    }
+
+    // Remove os "zeros à esquerda", ninguém gosta deles.
+    if (resultado->fim->valor == 0)
+    {
+        retira_elemento(resultado, resultado->len);
+    }
+
 }
 
 void multiplica_ngrande(lista_ligada_t *resultado, lista_ligada_t *n1, lista_ligada_t *n2)
 {
+    esvazia_lista(resultado);
+
     // TODO: Algoritmo de multiplicação aqui
 }
 
 void subtrai_ngrande(lista_ligada_t *resultado, lista_ligada_t *n1, lista_ligada_t *n2)
 {
+    esvazia_lista(resultado);
+
     // Garante que a subtração sempre seja feita em módulo.
     if (n1->len > n2->len) {
         copia_lista(resultado, n1);
@@ -88,5 +162,7 @@ void subtrai_ngrande(lista_ligada_t *resultado, lista_ligada_t *n1, lista_ligada
 
 void divide_ngrande(lista_ligada_t *resultado, lista_ligada_t *n1, lista_ligada_t *n2)
 {
+    esvazia_lista(resultado);
+
     // TODO: Algoritmo de divisão aqui
 }
