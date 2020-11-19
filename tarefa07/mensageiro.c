6b379ecca4564fa321424e8f138a43d5c27ea9f9
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-11-19 19:59:06 -0400

    Cria arquivo mensageiro.c

---
+++ tarefa07/mensageiro.c
@@ -0,0 +1,14 @@
+/**
+ * Daniel Paulo Garcia © 2020
+ *
+ * mensageiro.c
+ *
+ * Descrição: Recebe um conjunto de tokens numerados unicamente, cada um contendo um trecho de texto, além de uma
+ * sequência de inteiros usados para determinar a ordem de impressão dos tokens. O programa devolve a mensagem decodificada.
+ */
+
+int main(void)
+{
+    /* code */
+    return 0;
+}
