883a1943311537945923904e907f876d78dd4827
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-11-25 01:08:46 -0400

    Primeira tentativa (fracassada) de resolução da tarefa, pelo menos agora não mais erros ou memory leaks;

--- tarefa07/mensageiro.c
+++ tarefa07/mensageiro.c
@@ -46,8 +46,60 @@ nodo_rb * cria_nodo_cartao()
 
 void libera_nodo_cartao(nodo_rb *nd, void *uu)
 {
+    // A variável "uu" não é usada
     free((struct cartao *) nd->chave);
     free(nd);
+}
+
+void imprime_nodo(nodo_rb *nd, void* uu)
+{
+    // A variável "uu" não é usada
+    printf("%s", ((struct cartao*)nd->chave)->texto);
+}
+
+void processa_trio(arvore_rb *t, int soma)
+{
+    /* Basicamente resolve o problema da soma do subconjunto para o caso especial em que n=3 */
+    nodo_rb *n0, *n1, *n2;
+    nodo_rb *aux;
+
+    // Preferir alocação estática para variáveis temporárias
+    nodo_rb nd_soma;
+    struct cartao meu_cartao;
+    nd_soma.chave = &meu_cartao;
+
+    int *resto = &((struct cartao*) nd_soma.chave)->numero;
+
+    n0 = arvore_minimo(t);
+    while (n0 != ARVORE_NULL)
+    {
+        n1 = arvore_sucessor(n0);
+        while (n1 != ARVORE_NULL)
+        {
+            *resto = soma - ((struct cartao*) n0->chave)->numero - ((struct cartao*) n1->chave)->numero;
+            n2 = arvore_buscar(t, nd_soma.chave);
+
+            if (n2 != NULL)
+            {
+                imprime_nodo(n0, NULL);
+                imprime_nodo(n1, NULL);
+                imprime_nodo(n2, NULL);
+                libera_nodo_cartao(arvore_deletar(t, n0->chave), NULL);
+                libera_nodo_cartao(arvore_deletar(t, n1->chave), NULL);
+                libera_nodo_cartao(arvore_deletar(t, n2->chave), NULL);
+                goto fim;
+            }
+
+            n1 = arvore_sucessor(n1);
+        }
+
+        imprime_nodo(n0, NULL);
+        aux = n0;
+        n0 = arvore_sucessor(n0);
+        libera_nodo_cartao(arvore_deletar(t, aux->chave), NULL);
+    }
+
+    fim: ;
 }
 
 int ler_mensagem(arvore_rb *t)
@@ -58,7 +110,7 @@ int ler_mensagem(arvore_rb *t)
     int i;
 
     // Lê os inteiros m, n
-    if (scanf(" %d %d", &m, &n) == EOF) return EOF;
+    if (scanf("%d %d ", &m, &n) == EOF) return EOF;
 
     // Lê os cartões
     for (i = 0; i < m; i++)
@@ -73,10 +125,16 @@ int ler_mensagem(arvore_rb *t)
     }
 
     // Para cada "autoridade", imprime o trio.
+    int soma;
     for (i = 0; i < n; i++)
     {
-        // TODO: Algoritmo de achar trio aqui
+        scanf("%d", &soma);
+        processa_trio(t, soma);
     }
+
+    // Imprime o resto em ordem
+//    percorrer_inordem(t, imprime_nodo, NULL);
+    printf("\n");
 
     return 0;
 }
@@ -85,11 +143,16 @@ int main(void)
 {
     arvore_rb *t = arvore_criar(cmp_cartoes);
 
-    // Lê e decodifica cada mensagem enquanto não acabar a entrada, e libera a árvore após isso.
-    while (ler_mensagem(t) != EOF)
+    // Lê e decodifica cada mensagem enquanto não acabar a entrada, e depois libera os nós da árvore.
+    while (1)
     {
+        if (ler_mensagem(t) == EOF)
+            break;
+
         percorrer_posordem(t, libera_nodo_cartao, NULL);
+        arvore_inicializar(t, cmp_cartoes);
     }
 
+    free(t);
     return 0;
 }
