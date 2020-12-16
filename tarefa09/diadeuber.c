129a4b2e92719a807285ee6ee5774600d0ac7cc3
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-12-16 00:20:01 -0400

    Cria estruturas básicas do programa principal

--- tarefa09/diadeuber.c
+++ tarefa09/diadeuber.c
@@ -11,8 +11,35 @@
 
 #include "heapq.h"
 
+#define MAX_CLIENTES 256
+#define MAX_NOME 15
+
+struct cliente
+{
+    char nome[MAX_NOME];
+    double avaliacao;
+    int pos_origem[2];
+    int pos_destino[2];
+};
+
+int cmp_clientes(const void *c1, const void *c2)
+{
+    double a1 = ((struct cliente*) c1)->avaliacao;
+    double a2 = ((struct cliente*) c2)->avaliacao;
+
+    // É seguro comparar dois 'double' por igualdade nesse caso, pois os valores nunca são alterados algebricamente
+    if (a1 == a2)
+        return 0;
+    else if ( (a1 - a2) > 0)
+        return 1;
+    else
+        return -1;
+}
+
 int main(void)
 {
-    /* code */
+    heapq_t *fila_corridas = heapq_criar(MAX_CLIENTES, cmp_clientes);
+
+    heapq_destroi(fila_corridas);
     return 0;
 }
