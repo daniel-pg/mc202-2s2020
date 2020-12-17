3e258d102abe3ff7bdade18a5982df7fbedc9d52
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2020-12-17 08:20:24 -0400

    Cria loop principal do programa e implementa operações para aceitar solicitação e imprimir relatório

--- tarefa09/diadeuber.c
+++ tarefa09/diadeuber.c
@@ -7,9 +7,14 @@
  * corrida, bem como as estatísticas de quilômetros rodados, rendimento bruto e líquido e gastos ao fim do dia.
  */
 
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "heapq.h"
+
+#define XSTR(x) #x
+#define STR(x) XSTR(x)
 
 #define MAX_CLIENTES 256
 #define MAX_NOME 15
@@ -36,9 +41,85 @@ int cmp_clientes(const void *c1, const void *c2)
         return -1;
 }
 
+static void aceita_solicitacao(heapq_t *fila_corridas, struct cliente **cliente_atual, bool *motorista_ocupado)
+{
+    struct cliente *novo_cliente;
+    novo_cliente = malloc(sizeof(*novo_cliente) * MAX_CLIENTES);
+
+    scanf("%" STR(MAX_NOME) "s" "%lf %d %d %d %d",
+          novo_cliente->nome, &novo_cliente->avaliacao,
+          &novo_cliente->pos_origem[0], &novo_cliente->pos_origem[1],
+          &novo_cliente->pos_destino[0], &novo_cliente->pos_destino[1]);
+
+    // Se o motorista estiver ocupado, coloque o cliente na fila de espera. Caso contrário, atenda-o.
+    if (*motorista_ocupado){
+        heapq_inserir(fila_corridas, novo_cliente);
+    } else {
+        *cliente_atual = novo_cliente;
+        *motorista_ocupado = true;
+    }
+}
+
+static void imprime_relatorio_final(int km_total, double rend_bruto)
+{
+    // Constantes dadas no problema
+    const double custo_aluguel_diario = 57.0;
+    const double custo_por_km_rodado = 0.4104;
+
+    // Valores calculados
+    const double despesas = km_total * custo_por_km_rodado + custo_aluguel_diario;
+    const double rend_liquid = 0.75 * rend_bruto - despesas;
+
+    printf("Jornada finalizada. Aqui esta o seu rendimento de hoje");
+    printf("Km total: %d", km_total);
+    printf("Rendimento bruto: %2lf", rend_bruto);
+    printf("Despesas: %2lf", despesas);
+    printf("Rendimento liquido: %2lf", rend_liquid);
+}
+
+static void le_processa_entrada(heapq_t *fila_corridas)
+{
+    struct cliente *cliente_atual;
+    double rend_bruto = 0;
+    int km_total = 0;
+    char operacao;
+    bool motorista_ocupado = false;
+
+    do {
+        scanf("%c", &operacao);
+
+        switch (operacao)
+        {
+            case 'A':
+                /* O motorista aceitou a solicitação de um cliente, deixando-o em espera. */
+                aceita_solicitacao(fila_corridas, &cliente_atual, &motorista_ocupado);
+                break;
+
+            case 'F':
+                /* O motorista finalizou a corrida atual. */
+                break;
+
+            case 'C':
+                /* O cliente indicado cancelou sua solicitação. */
+                break;
+
+            case 'T':
+                /* O motorista indicou o término da sua jornada e deseja ver o rendimento obtido. */
+                imprime_relatorio_final(km_total, rend_bruto);
+                break;
+
+            default:
+                break;
+        }
+
+    } while (operacao != 'T');
+}
+
 int main(void)
 {
     heapq_t *fila_corridas = heapq_criar(MAX_CLIENTES, cmp_clientes);
+
+    le_processa_entrada(fila_corridas);
 
     heapq_destroi(fila_corridas);
     return 0;
