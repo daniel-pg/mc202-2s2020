493e4d9fb9d644ba61ce3a181d71042be1b874af
Daniel Paulo Garcia <d169374@dac.unicamp.br>
2021-01-03 00:31:50 -0400

    Converte módulo de lista ligada dupla para simples.
    Remove um monte de funções e valores que não estavam sendo utilizados no módulo de lista ligada.
    
    Adiciona um novo registro aos nós da lista para indicar se a palavra está grafada corretamente ou não.

--- tarefa10/lista_ligada.h
+++ tarefa10/lista_ligada.h
@@ -3,13 +3,14 @@
  *
  * lista_ligada.h
  *
- * Descrição: Interface genérica para uma estrutura de dados do tipo "lista duplamente ligada". A estrutura de dados é
+ * Descrição: Interface genérica para uma estrutura de dados do tipo "lista ligada simples". A estrutura de dados é
  * intrusiva, isto é, os valores são armazenados diretamente em cada nó da lista.
  */
 
 #ifndef TAREFA09_LISTA_LIGADA_H
 #define TAREFA09_LISTA_LIGADA_H
 
+#include <stdbool.h>
 #include <stddef.h>
 
 #define MAX_PALAVRA 26
@@ -17,95 +18,57 @@
 typedef struct celula_t
 {
     struct celula_t *prox;
-    struct celula_t *ant;
-    char valor[MAX_PALAVRA];
+
+    char chave[MAX_PALAVRA];
+    bool dado;
 } celula_t;
 
 typedef struct lista_ligada
 {
     celula_t *inicio;
-    celula_t *fim;
-    size_t len;
 } lista_ligada_t;
+
 
 /**
  * @brief Aloca e inicializa uma lista ligada na memória.
  * 
- * @return lista_ligada_t* 
+ * @return lista_ligada_t* Ponteiro para a nova lista, ou NULL caso a alocação falhe.
  */
 lista_ligada_t * lista_criar(void);
+
 
 /**
  * @brief Remove todos os elementos da lista.
  * 
- * @param lista 
+ * @param lista Ponteiro para a lista de onde se quer remover todos os seus elementos.
  */
 void lista_esvaziar(lista_ligada_t *lista);
+
 
 /**
  * @brief Esvazia a lista e logo em seguida libera-a da memória.
  * 
- * @param lista 
+ * @param lista Ponteiro para a lista que se quer destruir.
  */
 void lista_destruir(lista_ligada_t *lista);
 
-/**
- * @brief Insere elemento na posição anterior à especificada.
- * 
- * @param lista 
- * @param valor 
- * @param pos 
- */
-celula_t * lista_inserir(lista_ligada_t *lista, const char *valor, size_t pos);
 
 /**
- * @brief Anexa elemento ao início da lista.
+ * @brief Anexa elemento no início da lista.
  * 
- * @param lista 
- * @param valor 
- * @return celula_t* 
+ * @param lista Ponteiro para a lista onde se quer inserir o novo elemento.
+ * @param chave Valor de inicialização do novo elemento.
  */
-celula_t * lista_anexar_inicio(lista_ligada_t *lista, const char *valor);
+celula_t * lista_anexar_inicio(lista_ligada_t *lista, const char *chave, bool dado);
+
 
 /**
- * @brief Anexa elemento ao final da lista.
+ * @brief Remove primeira ocorrência do elemento especificado da lista e devolve o índice de sua posição.
  * 
- * @param lista 
- * @param valor 
+ * @param lista Ponteiro para a lista de onde se quer remover o elemento.
+ * @param chave Valor do elemento que se quer remover.
  */
-celula_t * lista_anexar_fim(lista_ligada_t *lista, const char *valor);
-
-/**
- * @brief Remove primeira ocorrência do elemento especificado da lista e devolve seu indice.
- * 
- * @param lista 
- * @param valor 
- */
-size_t lista_remover(lista_ligada_t *lista, const char *valor);
-
-/**
- * @brief Retira o elemento na posição especificada.
- * 
- * @param lista 
- * @param pos 
- */
-void lista_deletar(lista_ligada_t *lista, size_t pos);
-
-/**
- * @brief Concatena a lista de origem ao final da lista destino.
- * 
- * @param dest 
- * @param orig 
- */
-void lista_concatenar(lista_ligada_t *dest, lista_ligada_t *orig);
-
-/**
- * @brief Copia a lista de origem para a lista de destino.
- * 
- * @param dest 
- * @param orig 
- */
-void lista_copiar(lista_ligada_t *dest, lista_ligada_t *orig);
+size_t lista_remover(lista_ligada_t *lista, const char *chave);
 
 
 #endif //TAREFA09_LISTA_LIGADA_H
